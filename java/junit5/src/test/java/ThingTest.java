3710881b958d2c17a1cf5ebeab22c0bb3b9342ba
Xavier Moulin <xavier.moulin@soprasteria.com>
2020-05-21 10:13:31 +0200

    test1

--- java/junit5/src/test/java/ThingTest.java
+++ java/junit5/src/test/java/ThingTest.java
@@ -8,7 +8,24 @@ public class ThingTest {
     void fail() {
         Thing thing = new Thing();
         String value = thing.callForAction();
+        System.out.println();
+        System.out.println();
+        System.out.printf("zlkd");
+        System.out.println();
+
         assertEquals("Food", value);
+
+
+
+        for (int innn = 0; innn < ; innn++) {
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("coucou");
     }
 
     @Test
